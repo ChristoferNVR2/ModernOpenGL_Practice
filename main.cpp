905b24c7d1f6b6ad27f4844bc634863ff34f4e66
chrisvega <cnvegaro@unitru.edu.pe>
2024-10-09 19:21:47 -0500

    Add VAO

--- main.cpp
+++ main.cpp
@@ -109,6 +109,11 @@ int main() {
     /* Create a windowed mode window and its OpenGL context */
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
     glewInit();
 
     if (!window)
@@ -129,16 +134,20 @@ int main() {
 
 
     float positions[] = {
-        -0.5f, -0.5f, // 0
-         0.5f, -0.5f, // 1
-         0.5f,  0.5f, // 2
-        -0.5f,  0.5f  // 3
+        -0.5f, -0.5f, 0.5,// 0
+         0.5f, -0.5f, -0.5,// 1
+         0.5f,  0.5f, 0.5// 2
+        -0.5f,  0.5f,  0.5// 3
     };
 
     unsigned int indices[] = {
         0, 1, 2,
         2, 3, 0
     };
+
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
 
     unsigned int buffer;
     GLCall(glGenBuffers(1, &buffer));
@@ -164,6 +173,12 @@ int main() {
     ASSERT(location != -1);
     GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
 
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+
     float r = 0.0f;
     float increment = 0.05f;
 
@@ -172,7 +187,12 @@ int main() {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        GLCall(glBindVertexArray(vao));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr))
 
         if (r > 1.0f)
